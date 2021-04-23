91d1d5cddfa95b73fc41fd34146c1b5a3b4ee16c
Callum J Harding <callumhdg@gmail.com>
2021-04-23 14:53:17 +0100

    Added some Unit Tests and declared some Methods

--- Calculator_Back-End/Class1.cs
+++ Calculator_Back-End/Class1.cs
@@ -4,5 +4,30 @@ namespace Calculator_Back_End
 {
     public class Calculator
     {
+
+        public static double Add(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Multiply(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Divide(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
     }
 }
--- Calculator_Unit_Tests/UnitTest1.cs
+++ Calculator_Unit_Tests/UnitTest1.cs
@@ -1,18 +1,48 @@
 using NUnit.Framework;
+using Calculator_Back_End;
 
 namespace Calculator_Unit_Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+
+        [TestCase(3, 5, 8)]
+        [TestCase(83, 47, 130)]
+        [TestCase(64, -70, -6)]
+        public void AddMethodWorksAsIntended(double x, double y, double expected)
         {
+            double actual = Calculator.Add(x, y);
+            Assert.AreEqual(actual, expected);
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(7, 2, 5)]
+        [TestCase(194, 74, 120)]
+        [TestCase(55, -30, 85)]
+        public void SubtractMethodWorksAsIntended(double x, double y, double expected)
         {
-            Assert.Pass();
+            double actual = Calculator.Subtract(x, y);
+            Assert.AreEqual(actual, expected);
         }
+
+        [TestCase(6, 4, 24)]
+        [TestCase(8, 8, 64)]
+        [TestCase(100, 90, 9000)]
+        public void MultiplyMethodWorksAsIntended(double x, double y, double expected)
+        {
+            double actual = Calculator.Multiply(x, y);
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestCase(12, 4, 3)]
+        [TestCase(60, 15, 4)]
+        [TestCase(-90, 90, -1)]
+        public void DevideMethodWorksAsIntended(double x, double y, double expected)
+        {
+            double actual = Calculator.Divide(x, y);
+            Assert.AreEqual(actual, expected);
+        }
+
+
     }
 }
