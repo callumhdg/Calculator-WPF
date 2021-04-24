ca41097b4fd62394c9d8bbc4a60e77fb95dc22f3
Callum J Harding <callumhdg@gmail.com>
2021-04-24 11:08:41 +0100

    Base Unit Tests now pass and Class1.cs was renamed Calculator.cs

--- Calculator_Back-End/Calculator.cs
+++ Calculator_Back-End/Calculator.cs
@@ -7,22 +7,26 @@ namespace Calculator_Back_End
 
         public static double Add(double x, double y)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return x + y;
         }
 
         public static double Subtract(double x, double y)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return x - y;
         }
 
         public static double Multiply(double x, double y)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return x * y;
         }
 
         public static double Divide(double x, double y)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return x / y;
         }
 
 
