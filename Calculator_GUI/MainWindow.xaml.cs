2384ed50101018ba62aaed28f1dfee89d8ffddf8
Callum J Harding <callumhdg@gmail.com>
2021-04-25 13:15:05 +0100

    Improved BODMAS functionality, requires fix to logic. Currently if there are two completely seperate sums to calculate e.g., (2+4-10X2) then the sum will not calculate correctly.

--- Calculator_GUI/MainWindow.xaml.cs
+++ Calculator_GUI/MainWindow.xaml.cs
@@ -172,6 +172,9 @@ namespace Calculator_GUI
                 input.Add(item);
             }
 
+
+            //Sort opperations into BODMAS order
+            //1 + 1 becomes, 0 (opperation index), 0 (_nums index left number), "+" (opperator), 1 (_nums index right number)
             while (count < input.Count)
             {
                 if (input.Contains('/'))
@@ -180,7 +183,7 @@ namespace Calculator_GUI
                     {
                         if (input[i].ToString() == "/")
                         {
-                            BODMAS.Append(i + "/");
+                            BODMAS.Append(i.ToString() + i.ToString() + "/" + (i + 1));
                             count++;
                             divCount++;
                         }
@@ -192,7 +195,7 @@ namespace Calculator_GUI
                     {
                         if (input[i].ToString() == "X")
                         {
-                            BODMAS.Append(i + "X");
+                            BODMAS.Append(i.ToString() + i.ToString() + "X" + (i + 1));
                             count++;
                             mulCount++;
                         }
@@ -204,7 +207,7 @@ namespace Calculator_GUI
                     {
                         if (input[i].ToString() == "+")
                         {
-                            BODMAS.Append(i + "+");
+                            BODMAS.Append(i.ToString() + i.ToString() + "+" + (i + 1));
                             count++;
                             addCount++;
                         }
@@ -216,7 +219,7 @@ namespace Calculator_GUI
                     {
                         if (input[i].ToString() == "-")
                         {
-                            BODMAS.Append(i + "-");
+                            BODMAS.Append(i.ToString() + i.ToString() + "-" + (i + 1));
                             count++;
                             minCount++;
                         }
@@ -230,12 +233,12 @@ namespace Calculator_GUI
 
             if (divCount > 0)
             {
-                for (int j = 0; j < BODMAS.Length; j += 2)
+                for (int j = 0; j < BODMAS.Length; j += 4)
                 {
-                    if (BODMAS[j + 1].ToString() == "/")
+                    if (BODMAS[j + 2].ToString() == "/")
                     {
-                        int num1 = BODMAS[j] - 48;
-                        int num2 = BODMAS[j] - 47;
+                        int num1 = BODMAS[j + 1] - 48;
+                        int num2 = BODMAS[j + 3] - 48;
 
                         result += Calculator.Divide(Convert.ToInt32(_nums[num1]), Convert.ToInt32(_nums[num2]));
                         hasDoneMath = true;
@@ -244,19 +247,19 @@ namespace Calculator_GUI
             }
             if (mulCount > 0)
             {
-                for (int j = 0; j < BODMAS.Length; j += 2)
+                for (int j = 0; j < BODMAS.Length; j += 4)
                 {
-                    if (BODMAS[j + 1].ToString() == "X" && hasDoneMath == false)
+                    if (BODMAS[j + 2].ToString() == "X" && hasDoneMath == false)
                     {
-                        int num1 = BODMAS[j] - 48;
-                        int num2 = BODMAS[j] - 47;
+                        int num1 = BODMAS[j + 1] - 48;
+                        int num2 = BODMAS[j + 3] - 48;
 
                         result += Calculator.Multiply(Convert.ToInt32(_nums[num1]), Convert.ToInt32(_nums[num2]));
                         hasDoneMath = true;
                     }
-                    else if (BODMAS[j + 1].ToString() == "X" && hasDoneMath == true)
+                    else if (BODMAS[j + 2].ToString() == "X" && hasDoneMath == true)
                     {
-                        int num1 = BODMAS[j] - 48;
+                        int num1 = (BODMAS[j - 2].ToString() == "X") ? BODMAS[j + 3] - 48 : BODMAS[j + 1] - 48;
                         int num2 = result;
 
                         result += Calculator.Multiply(Convert.ToInt32(_nums[num1]), num2);
@@ -266,19 +269,19 @@ namespace Calculator_GUI
             }
             if (addCount > 0)
             {
-                for (int j = 0; j < BODMAS.Length; j += 2)
+                for (int j = 0; j < BODMAS.Length; j += 4)
                 {
-                    if (BODMAS[j + 1].ToString() == "+" && hasDoneMath == false)
+                    if (BODMAS[j + 2].ToString() == "+" && hasDoneMath == false)
                     {
-                        int num1 = BODMAS[j] - 48;
-                        int num2 = BODMAS[j] - 47;
+                        int num1 = BODMAS[j + 1] - 48;
+                        int num2 = BODMAS[j + 3] - 48;
 
                         result += Calculator.Add(Convert.ToInt32(_nums[num1]), Convert.ToInt32(_nums[num2]));
                         hasDoneMath = true;
                     }
-                    else if (BODMAS[j + 1].ToString() == "+" && hasDoneMath == true)
-                    {
-                        int num1 = BODMAS[j] - 48;
+                    else if (BODMAS[j + 2].ToString() == "+" && hasDoneMath == true)
+                    {                        
+                        int num1 = (BODMAS[j - 2].ToString() == "+") ? BODMAS[j + 3] - 48 : BODMAS[j + 1] - 48;
                         int num2 = result;
 
                         result = Calculator.Add(Convert.ToInt32(_nums[num1]), num2);
@@ -290,29 +293,34 @@ namespace Calculator_GUI
 
             if (minCount > 0)
             {
-                for (int j = 0; j < BODMAS.Length; j += 2)
+                for (int j = 0; j < BODMAS.Length; j += 4)
                 {
-                    if (BODMAS[j + 1].ToString() == "-" && hasDoneMath == false)
+                    if (BODMAS[j + 2].ToString() == "-" && hasDoneMath == false)
                     {
-                        int num1 = BODMAS[j] - 48;
-                        int num2 = BODMAS[j] - 47;
+                        int num1 = BODMAS[j + 1] - 48;
+                        int num2 = BODMAS[j + 3] - 48;
 
                         result += Calculator.Subtract(Convert.ToInt32(_nums[num1]), Convert.ToInt32(_nums[num2]));
                     }
-                    else if (BODMAS[j + 1].ToString() == "-" && hasDoneMath == true)
+                    else if (BODMAS[j + 2].ToString() == "-" && hasDoneMath == true)
                     {
-                        int num1 = BODMAS[j] - 48;
+                        int num1 = (BODMAS[j - 2].ToString() == "-") ? BODMAS[j + 3] - 48 : BODMAS[j + 1] - 48;
                         int num2 = result;
 
-                        result = Calculator.Subtract(Convert.ToInt32(_nums[num1]), num2);
+                        //result = Calculator.Subtract(Convert.ToInt32(_nums[num1]), num2);
+                        //result = () ? Calculator.Subtract(Convert.ToInt32(_nums[num1]), num2) : Calculator.Subtract(Convert.ToInt32(num2, _nums[num1]));
+                        result = Calculator.Subtract(num2, Convert.ToInt32(_nums[num1]));
+
                         hasDoneMath = true;
                     }
                 }
             }
 
-            lblDisplay.Content = result;
-            _selectedOpperation.Clear();
+             Clear();
+            _nums[0] = result.ToString();
 
+            lblDisplay.Content = result;
+            _selectedOpperation.Clear();           
         }
 
 
@@ -320,16 +328,21 @@ namespace Calculator_GUI
         //Clear
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
+            Clear();
+        }
+
+        private void Clear()
+        {
             _currentNum.Clear();
             _currentNumCount = 0;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 1; i < 9; i++)
             {
                 _nums[i] = null;
             }
 
             _selectedOpperation.Clear();
-            lblDisplay.Content = "";
+            lblDisplay.Content = "0";
         }
 
 
@@ -340,7 +353,7 @@ namespace Calculator_GUI
             StringBuilder displayOut = new StringBuilder();
             int counter = 0;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 9; i++)
             {
                 if (_nums[i] != null)
                 {
